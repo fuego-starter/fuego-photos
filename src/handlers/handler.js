69acabf105a7424a3ad4389478939d511b8d5498
Reltre <tglippert@gmail.com>
2018-01-18 18:44:48 -0800

    Move handler to dedicated folder and update with TypeScript static typing.

---
+++ src/handlers/handler.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.helloWorld = function (event, context, callback) {
+    var response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Hello World!',
+            input: event
+        })
+    };
+    callback(null, response);
+};
