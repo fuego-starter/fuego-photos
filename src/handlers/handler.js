4857ad380d75098a2a529ef56acef668037f3d5a
Reltre <tglippert@gmail.com>
2018-01-26 14:18:18 -0800

    Update serverless configuration file with better function name. Update helloName handler function to accept any event type.

--- src/handlers/handler.js
+++ src/handlers/handler.js
@@ -10,3 +10,13 @@ exports.helloWorld = function (event, context, callback) {
     };
     callback(null, response);
 };
+exports.helloUser = function (event, context, callback) {
+    var response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Hello " + event.name + "!",
+            input: event
+        })
+    };
+    callback(null, response);
+};
