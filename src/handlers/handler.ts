d0e80bac989c04a22e3a7f7a2bc52fe3a0f13f2b
Reltre <tglippert@gmail.com>
2018-03-10 18:30:36 -0800

    Initial writeup. Create configuration for cognito trigger function. Boilerplate code for user storage handler function.

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,5 +1,7 @@
-import { APIGatewayEvent, ProxyCallback} from 'aws-lambda'
+import { APIGatewayEvent,CognitoUserPoolEvent, ProxyCallback} from 'aws-lambda'
 import { Context, ProxyHandler, ProxyResult } from 'aws-lambda';
+// import aws-sdk to access s3 storage
+
 
 export const helloWorld: ProxyHandler = (event: APIGatewayEvent, context: Context, callback: ProxyCallback) => {
   const response: ProxyResult = {
@@ -23,5 +25,11 @@ export const helloUser: ProxyHandler = (event: APIGatewayEvent, context: Context
     }),
   };
 
+  callback(null, response);
+}
+
+export const provisionUserDataStorage: ProxyHandler = (event: CognitoUserPoolEvent, context: Context, callback: ProxyCallback) => {
+  // Verify user in pool, then setup storage on s3
+  let response: ProxyResult; // Needs setup and assignment
   callback(null, response);
 }
