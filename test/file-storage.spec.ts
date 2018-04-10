c69fdf37f381c604a694870677fe356b234f01ea
Reltre <tglippert@gmail.com>
2018-04-10 16:30:53 -0700

    Add fixtures for callback parameters used in s3 call.

--- test/file-storage.spec.ts
+++ test/file-storage.spec.ts
@@ -4,11 +4,11 @@ import * as sinon from 'sinon';
 import { FileStorage } from '../src/lib/file-storage';
 
 describe('creating a user folder on s3', function() {
-  it('creates a folder for a newly registered user to store data', async function() {
+  it('creates an object on s3 given a set of parameters', function() {
     let email = 'example@example.com'
-    let fileStorage = new FileStorage({env: 'test'});
+    let fileStorage = new FileStorage();
     let params = { Bucket: 'fuegophotos', Key: `${email}/`};
-    await fileStorage.S3.putObject(params);
+    fileStorage.S3.putObject(params);
     let stubPutObject: sinon.SinonStub = <sinon.SinonStub> fileStorage.S3.putObject;
     stubPutObject.restore();
     sinon.assert.calledWith(stubPutObject.firstCall, params);
--- test/registration.spec.ts
+++ test/registration.spec.ts
@@ -1,32 +1,56 @@
 import 'mocha';
 import { provisionUserDataStorage } from '../src/handlers/handler';
-import { expect } from "chai";
+import { expect, assert } from "chai";
 import * as sinon from 'sinon';
 import * as fs from 'fs';
-import { StubContext, StubEvent } from './stubs/hello';
+import { FileStorage } from '../src/lib/file-storage' 
+import { StubContext } from './stubs/hello';
 import { CognitoUserPoolEvent, Callback, APIGatewayEvent } from 'aws-lambda';
 
 
-describe.skip('provisionUserDataStorage', function() {
+describe('provisionUserDataStorage', function() {
 
   let context: StubContext = new StubContext('provisionUserDataStorage');
   let event: CognitoUserPoolEvent;
-  it('creates a folder for a newly registered user to store data', async function() {
+  it("it returns valid data when a user folder is created", function() {
     // mock s3 object folder creation workflow
     // ensure call to folder creation takes an argument of user email
     // calls: new AWS.S3; s3.upload
     let fileData: string;
     fileData = fs.readFileSync('./test/fixtures/post-confirmation-user-pool.json', 'utf8');
     event = JSON.parse(fileData);
+    let fileStorage = new FileStorage();
+    let params = { Bucket: 'fuego-photos-users', Key: `${event.userName}/` }
+    setEnv();
+    provisionUserDataStorage(event, context, function(err, data) {
+      
+    });
+    // fileStorage.S3.getObject(params, (err, data) => {
+    //   expect(data.ContentLength).to.be.greaterThan(0);
+    // });
+  });
 
-    // let AWS = new FileStorage;
-
-
-    // let params = { Bucket: 'fuegophotos', Key: `${event.userName}/` }
-
-    // await provisionUserDataStorage(event, context, (err, result) => {});
-    // let saveObject: sinon.SinonStub = <sinon.SinonStub> AWS.S3.putObject;
-    // saveObject.restore();
-    // sinon.assert.calledOnce(saveObject);
+  it("it returns an error when a user folder can't be created.", function() {
+    // mock s3 object folder creation workflow
+    // ensure call to folder creation takes an argument of user email
+    // calls: new AWS.S3; s3.upload
+    let fileData: string;
+    fileData = fs.readFileSync('./test/fixtures/post-confirmation-user-pool.json', 'utf8');
+    event = JSON.parse(fileData);
+    let fileStorage = new FileStorage();
+    let params = { Bucket: 'fuego-photos-users', Key: `${event.userName}/` }
+    setEnv();
+    provisionUserDataStorage(event, context, function(err, data) {
+      
+    });
+    // fileStorage.S3.getObject(params, (err, data) => {
+    //   expect(data.ContentLength).to.be.greaterThan(0);
+    // });
   });
 });
+
+
+
+function setEnv() {
+  process.env.NODE_ENV = '_test';
+}
